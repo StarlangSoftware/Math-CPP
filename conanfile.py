da08365096ae6c4e0b69a4ca5d654f2d4da3f113
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-08-21 18:37:35 +0300

    Added Conan packaging system.

--- conanfile.py
+++ conanfile.py
@@ -25,7 +25,7 @@ class MathConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="")
+        self.copy("*.h", dst="include", keep_path=False)
         self.copy("*Math.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
